f8db66e8ef3f910f3fbd01f7de4272277044e70f
PREETI SARDAR <82765024+PretulikaSardaris@users.noreply.github.com>
2024-03-19 12:34:56 +0000

    jsCoversion

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = 33;
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+/*'33' => 33;
+'33abc' => Nan;
+true => 1, false => 0; is a number
+
+
+*/
+
+let loggedIn = 1;
+let booleanIsLoggedIn = Boolean(loggedIn)
+//console.log(booleanIsLoggedIn);
+
+let myContact = 49
+let stringContact = String(myContact)
+
+console.log(typeof stringContact);
