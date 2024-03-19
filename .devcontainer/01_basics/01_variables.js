1a9ef60f0715185060e89a9df6c78749acada574
PREETI SARDAR <82765024+PretulikaSardaris@users.noreply.github.com>
2024-03-19 10:43:20 +0000

    learning variable

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144554
+let accountEmail = 'preetisaradar@gmail.com'
+var accountPaasword = '1254'
+accountCity = 'pune'
+
+//accountId = 546221
+/*
+prefer not to use var, cy issue of block and functional scope
+ try using const and let
+*/
+console.log(accountId);
+console.table([accountEmail,accountId,accountPaasword,accountCity])
